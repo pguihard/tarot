1851666585d471608a593e01e62acf6426c03937
Phiippe Guihard <philippe.guihard@cegetel.net>
2025-12-01 10:43:46 +0100

    202512011043

--- scripts/score.js
+++ scripts/score.js
@@ -1,9 +1,31 @@
 const preneur = JSON.parse(localStorage.getItem("preneur"))
 console.log("score.js: Preneur sélectionné :", preneur);
 
-const display = document.getElementById('preneur');
-display.innerHTML = `
-  <h2>Preneur : ${preneur}</h2>
+const display1 = document.getElementById('preneur');
+display1.innerHTML = `
+  <h4>Preneur : ${preneur}</h4>
+`;
+
+const playerNames = JSON.parse(localStorage.getItem("playerNames")) || [];
+
+const display2 = document.getElementById('petitaubout');
+display2.innerHTML = `
+  <h4>Un des joueurs a mené le petit au bout :</h4>
+  <form id="choixPreneur">
+    <ul>
+      ${playerNames
+        .map(
+          (name) => `
+            <li>
+              <label>
+                <input type="radio" name="petitaubout" value="${name}">
+                ${name}
+              </label>
+            </li>`
+        )
+        .join('')}
+    </ul>
+  </form>
 `;
 
 function lireContrat() {
@@ -17,18 +39,16 @@ function lireContrat() {
             break;
         }
     }
-
     // Récupère le nombre de bouts
     var nbBouts = parseInt(document.getElementById("nbBouts").value, 10);
-
     // Vérification du nombre de bouts
     if (isNaN(nbBouts) || nbBouts < 0 || nbBouts > 3) {
         document.getElementById("resultat").innerHTML =
             "Erreur : le nombre de bouts doit être entre 0 et 3.";
         return;
     }
-
     // Affiche le résultat
     document.getElementById("resultat").innerHTML =
             "Contrat choisi : " + choix + "<br>Nombre de bouts : " + nbBouts;
+
 }    
